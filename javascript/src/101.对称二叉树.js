c7ad3e2a5f5c32a8092fd77e71f8f454dd61cc35
Ivonhoe <yangfan3687@163.com>
2020-06-27 22:03:31 +0800

    二叉树

---
+++ javascript/src/101.对称二叉树.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=javascript
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (52.26%)
+ * Likes:    876
+ * Dislikes: 0
+ * Total Accepted:    166.3K
+ * Total Submissions: 318.1K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 
+ * 
+ * 
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 
+ * 
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    return solution2(root)
+};
+
+/**
+ * 层序遍历
+ * @param {boolean} root 
+ */
+function solution1 (root) {
+    return false
+}
+
+function solution2 (root) {
+    if (root == null) {
+        return true
+    }
+
+    return recur(root.left, root.right)
+}
+
+function recur (left, right) {
+    if (left == null && right == null)
+        return true
+    if (left == null || right == null || left.val != right.val)
+        return false
+    return recur(left.left, right.right) && recur(left.right, right.left)
+}
+// @lc code=end
+
