cb98f8ea3a124b8a7f918df5391c3656f27ae0c7
Ivonhoe <yangfan3687@163.com>
2020-07-01 00:02:25 +0800

    斐波那契

---
+++ javascript/src/70.爬楼梯.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (49.81%)
+ * Likes:    1100
+ * Dislikes: 0
+ * Total Accepted:    231.3K
+ * Total Submissions: 463.9K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 注意：给定 n 是一个正整数。
+ * 
+ * 示例 1：
+ * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    var data = []
+    data[0] = 1, data[1] = 1
+    for (var i = 2; i <= n; i++) {
+        data[i] = data[i - 1] + data[i - 2]
+    }
+
+    return data[n]
+};
+// @lc code=end
+
