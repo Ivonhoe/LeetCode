c8e57f008e4c5b7dfcf1625b681bdbd73cb006b5
Ivonhoe <yangfan3687@163.com>
2020-10-13 23:01:38 +0800

    dfs

---
+++ javascript/src/79.单词搜索.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=79 lang=javascript
+ *
+ * [79] 单词搜索
+ *
+ * https://leetcode-cn.com/problems/word-search/description/
+ *
+ * algorithms
+ * Medium (41.78%)
+ * Likes:    469
+ * Dislikes: 0
+ * Total Accepted:    68.2K
+ * Total Submissions: 162.4K
+ * Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+ *
+ * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+ * 
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * board =
+ * [
+ * ⁠ ['A','B','C','E'],
+ * ⁠ ['S','F','C','S'],
+ * ⁠ ['A','D','E','E']
+ * ]
+ * 
+ * 给定 word = "ABCCED", 返回 true
+ * 给定 word = "SEE", 返回 true
+ * 给定 word = "ABCB", 返回 false
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * board 和 word 中只包含大写和小写英文字母。
+ * 1 <= board.length <= 200
+ * 1 <= board[i].length <= 200
+ * 1 <= word.length <= 10^3
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (dfs(board, word, i, j, 0)) {
+                return true
+            }
+        }
+    }
+
+    return false
+};
+
+/**
+ * 
+ * @param {*} board 
+ * @param {string} word
+ * @param {int} i 坐标 
+ * @param {int} j 坐标
+ * @param {int} k 当前word index 
+ */
+
+// temp是使用！！！
+var dfs = function (board, word, i, j, k) {
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || word[k] != board[i][j]) {
+        return false
+    }
+
+    if (k == word.length - 1) {
+        return true
+    }
+
+    var temp = board[i][j]
+    board[i][j] = '/'
+    var result = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) || dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1)
+    board[i][j] = temp
+    return result
+}
+// @lc code=end
