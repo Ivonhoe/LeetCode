199b0a0cf1852c0c438a62e35210e302a32befec
Ivonhoe <yangfan3687@163.com>
2021-01-31 23:49:27 +0800

    二叉树遍历

---
+++ kotlin/src/leetcode/editor/cn/124-BinaryTreeMaximumPathSum.kt
@@ -0,0 +1,69 @@
+import leetcode.editor.bean.TreeNode
+
+//路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。该路径 至少包含一个 节点，且不一定经过根节点。
+//
+// 路径和 是路径中各节点值的总和。 
+//
+// 给你一个二叉树的根节点 root ，返回其 最大路径和 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3]
+//输出：6
+//解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [-10,9,20,null,null,15,7]
+//输出：42
+//解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围是 [1, 3 * 104] 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 867 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxPathSum(root: TreeNode?): Int {
+        var ans = Integer.MIN_VALUE
+
+        fun onSideMax(node: TreeNode?): Int {
+            if (node == null) {
+                return 0
+            }
+
+            var left = Math.max(0, onSideMax(node.left))
+            var right = Math.max(0, onSideMax(node.right))
+
+            ans = Math.max(ans, left + right + node.`val`)
+            return Math.max(left, right) + node.`val`
+        }
+
+        onSideMax(root)
+        return ans
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
