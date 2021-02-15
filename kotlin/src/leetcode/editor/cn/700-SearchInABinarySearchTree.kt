10c8c575e4c5ae4b227fa9b6995676c50a878c07
Ivonhoe <yangfan3687@163.com>
2021-02-16 00:14:56 +0800

    BST搜索

---
+++ kotlin/src/leetcode/editor/cn/700-SearchInABinarySearchTree.kt
@@ -0,0 +1,57 @@
+import leetcode.editor.bean.TreeNode
+
+//给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+//
+// 例如， 
+//
+// 
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和值: 2
+// 
+//
+// 你应该返回如下子树: 
+//
+// 
+//      2     
+//     / \   
+//    1   3
+// 
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。 
+// Related Topics 树 
+// 👍 111 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) {
+            return null
+        }
+
+        if (root?.`val` == `val`) {
+            return root
+        } else if (root?.`val` > `val`) {
+            return searchBST(root?.left, `val`)
+        } else
+            return searchBST(root?.right, `val`)
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
