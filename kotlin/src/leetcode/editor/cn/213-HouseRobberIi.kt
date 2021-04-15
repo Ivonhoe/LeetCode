9d40a8134f63225964e6622922bc3987521d6a3f
Ivonhoe <yangfan3687@163.com>
2021-04-16 00:06:57 +0800

    每日一题

--- kotlin/src/leetcode/editor/cn/213-HouseRobberIi.kt
+++ kotlin/src/leetcode/editor/cn/213-HouseRobberIi.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.math.max
 
 //你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的
 //房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。 
@@ -51,7 +52,7 @@ class Solution {
         if (nums.size == 1) {
             return nums[0]
         }
-        return Math.max(rob2(Arrays.copyOfRange(nums, 0, nums.size - 1)), rob2(Arrays.copyOfRange(nums, 1, nums.size)))
+        return Math.max(rob2(nums.copyOfRange(0, nums.size - 1)), rob2(nums.copyOfRange(1, nums.size)))
     }
 
     fun rob2(nums: IntArray): Int {
