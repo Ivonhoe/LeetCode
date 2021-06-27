17730a096d3958bdb71f8595017ddf8f7b971e85
Ivonhoe <yangfan3687@163.com>
2021-06-27 19:09:57 +0800

    插入排序

---
+++ java/src/147.对链表进行插入排序.java
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=java
+ *
+ * [147] 对链表进行插入排序
+ *
+ * https://leetcode-cn.com/problems/insertion-sort-list/description/
+ *
+ * algorithms
+ * Medium (67.64%)
+ * Likes:    402
+ * Dislikes: 0
+ * Total Accepted:    90.7K
+ * Total Submissions: 134K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 对链表进行插入排序。
+ * 
+ * 
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ * 
+ * 
+ * 
+ * 插入排序算法：
+ * 
+ * 
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode ans = new ListNode(Integer.MIN_VALUE, null);
+        ListNode p = head;
+        while(p!=null){
+            ListNode temp = p.next;
+
+            // 找插入点
+            ListNode q = ans;
+            while(q.next!=null && p.val>=q.next.val){
+                q = q.next;
+            }
+
+            // insert
+            p.next = q.next;
+            q.next = p;
+
+            p = temp;
+        }
+
+        return ans.next;
+    }
+}
+// @lc code=end
+
