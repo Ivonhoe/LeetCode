b7fe073e3918f439e15d9213acc8515a0e5e9bb4
Ivonhoe <yangfan3687@163.com>
2021-09-12 23:36:28 +0800

    二叉树

---
+++ java/src/236.二叉树的最近公共祖先.java
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=java
+ *
+ * [236] 二叉树的最近公共祖先
+ *
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (68.02%)
+ * Likes:    1290
+ * Dislikes: 0
+ * Total Accepted:    250.6K
+ * Total Submissions: 368.5K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * 
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x
+ * 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出：3
+ * 解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出：5
+ * 解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1,2], p = 1, q = 2
+ * 输出：1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [2, 10^5] 内。
+ * -10^9 
+ * 所有 Node.val 互不相同 。
+ * p != q
+ * p 和 q 均存在于给定的二叉树中。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    TreeNode ans;
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return ans;
+    }
+    
+    private int dfs(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null){
+            return 0;
+        }
+        
+        int leftContain = dfs(root.left, p, q);
+        if(ans != null){
+            return 2;
+        }
+        
+        int rightContain = dfs(root.right, p, q);
+        if(ans != null){
+            return 2;
+        }
+        
+        int rootContain = 0;
+        if(root == p || root == q){
+            rootContain=1;
+        }
+        if(rootContain==0 && leftContain==1 && rightContain==1){
+            ans = root;
+        }
+        if(rootContain==1 && (leftContain==1 || rightContain==1)){
+            ans = root;
+        }
+        
+        return leftContain + rightContain + rootContain;
+    }
+}
+// @lc code=end
+
