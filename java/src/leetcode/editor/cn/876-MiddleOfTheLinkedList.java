916b52af6df9ab9e4b75a73f534b0d45f6467ebb
Ivonhoe <yangfan3687@163.com>
2022-02-19 23:52:06 +0800

    双指针

---
+++ java/src/leetcode/editor/cn/876-MiddleOfTheLinkedList.java
@@ -0,0 +1,69 @@
+import ivonhoe.java.leetcode.bean.ListNode;
+
+/*
+ * @lc app=leetcode.cn id=876 lang=java
+ *
+ * [876] Middle of the Linked List
+ *
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.61%)
+ * Likes:    489
+ * Dislikes: 0
+ * Total Accepted:    200.2K
+ * Total Submissions: 283.4K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, return the middle node of the linked
+ * list.
+ * 
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we
+ * return the second one.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is in the range [1, 100].
+ * 1 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+// @lc code=end
