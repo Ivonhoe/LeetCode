bdf7263d8c1e27d922c53c5103e14decba8d2d58
Ivonhoe <yangfan3687@163.com>
2022-02-21 00:12:58 +0800

    二分

--- java/src/leetcode/editor/cn/540-SingleElementInASortedArray.java
+++ java/src/leetcode/editor/cn/540-SingleElementInASortedArray.java
@@ -42,9 +42,58 @@ package leetcode.editor.cn;
 
 // @lc code=start
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+    public int singleNonDuplicate(final int[] nums) {
+        return solution2(nums);
+    }
 
+    /**
+     * 有序数组，二分
+     * https://leetcode-cn.com/problems/single-element-in-a-sorted-array/solution/gong-shui-san-xie-er-duan-xing-fen-xi-yu-17nv/
+     * 
+     * @param nums
+     * @return
+     */
+    private int solution2(final int[] nums) {
+        int low = 0, high = nums.length - 1;
+        int n = nums.length;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 0) {
+                if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+                    // 在右边
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (mid - 1 >= 0 && nums[mid - 1] == nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return nums[low];
+    }
+
+    /**
+     * 步长为2向前循环检查，时间复杂度O(N)
+     * 
+     * @param nums
+     * @return
+     */
+    private int solution1(final int[] nums) {
+        int i = 0, j = 1;
+        while (i < nums.length && j < nums.length) {
+            if (nums[i] == nums[j]) {
+                i = i + 2;
+                j = j + 2;
+                continue;
+            } else {
+                return nums[i];
+            }
+        }
+        return nums[i];
     }
 }
 // @lc code=end
-
