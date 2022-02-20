a3ecf448b479e1edd38236fe9eeb1fec4e4840f6
Ivonhoe <yangfan3687@163.com>
2022-02-20 15:55:36 +0800

    数组翻转

---
+++ java/src/leetcode/editor/cn/189-RotateArray.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode-cn.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Medium (44.53%)
+ * Likes:    1333
+ * Dislikes: 0
+ * Total Accepted:    431.8K
+ * Total Submissions: 969.5K
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation: 
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 0 <= k <= 10^5
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * Try to come up with as many solutions as you can. There are at least three
+ * different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+// @lc code=end
