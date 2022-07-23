3f59d8ad24ca9c26dee77dd158ef1800a5194b5e
Ivonhoe <yangfan3687@163.com>
2022-07-23 10:48:51 +0800

    二分

---
+++ java/src/leetcode/editor/cn/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,89 @@
+package leetcode.editor.cn;
+
+/*
+ * @lc app=leetcode.cn id=81 lang=java
+ *
+ * [81] Search in Rotated Sorted Array II
+ *
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Medium (41.43%)
+ * Likes:    543
+ * Dislikes: 0
+ * Total Accepted:    140.3K
+ * Total Submissions: 338.6K
+ * Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+ *
+ * There is an integer array nums sorted in non-decreasing order (not
+ * necessarily with distinct values).
+ * 
+ * Before being passed to your function, nums is rotated at an unknown pivot
+ * index k (0 <= k < nums.length) such that the resulting array is [nums[k],
+ * nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+ * For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and
+ * become [4,5,6,6,7,0,1,2,4,4].
+ * 
+ * Given the array nums after the rotation and an integer target, return true
+ * if target is in nums, or false if it is not in nums.
+ * 
+ * You must decrease the overall operation steps as much as possible.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [2,5,6,0,0,1,2], target = 0
+ * Output: true
+ * Example 2:
+ * Input: nums = [2,5,6,0,0,1,2], target = 3
+ * Output: false
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 5000
+ * -10^4 <= nums[i] <= 10^4
+ * nums is guaranteed to be rotated at some pivot.
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ * 
+ * Follow up: This problem is similar to Search in Rotated Sorted Array, but
+ * nums may contain duplicates. Would this affect the runtime complexity? How
+ * and why?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (target == nums[middle]) {
+                return true;
+            }
+
+            if (nums[left] == nums[middle] && nums[right] == nums[middle]) {
+                left += 1;
+                right -= 1;
+            } else if (nums[left] <= nums[middle]) {
+                if (target >= nums[left] && target < nums[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else {
+                if (target <= nums[right] && target > nums[middle]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
+// @lc code=end
