1c1a126423c4f249e1cc27b6b6fc64b46b9aef5c
Ivonhoe <yangfan3687@163.com>
2022-08-07 11:20:45 +0800

    hashmap，位运算

---
+++ java/src/leetcode/editor/cn/136-SingleNumber.java
@@ -0,0 +1,76 @@
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=136 lang=java
+ *
+ * [136] Single Number
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (72.07%)
+ * Likes:    2520
+ * Dislikes: 0
+ * Total Accepted:    746.9K
+ * Total Submissions: 1M
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers nums, every element appears twice except
+ * for one. Find that single one.
+ * 
+ * You must implement a solution with a linear runtime complexity and use only
+ * constant extra space.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [2,2,1]
+ * Output: 1
+ * Example 2:
+ * Input: nums = [4,1,2,1,2]
+ * Output: 4
+ * Example 3:
+ * Input: nums = [1]
+ * Output: 1
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 3 * 10^4
+ * -3 * 10^4 <= nums[i] <= 3 * 10^4
+ * Each element in the array appears twice except for one element which appears
+ * only once.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+
+    private int solution2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : nums) {
+            Integer count = map.get(i);
+            count = count == null ? 1 : ++count;
+            map.put(i, count);
+        }
+        for (Integer i : map.keySet()) {
+            Integer count = map.get(i);
+            if (count == 1) {
+                return i;
+            }
+        }
+        return -1; // can't find it.
+    }
+}
+// @lc code=end
