ff8ff2e2f20fb0c37384c40d8e1807ad297e48a3
Ivonhoe <yangfan3687@163.com>
2022-12-23 17:52:07 +0800

    二叉树

---
+++ java/src/leetcode/editor/cn/226-InvertBinaryTree.java
@@ -0,0 +1,74 @@
+package leetcode.editor.cn;
+
+import ivonhoe.java.leetcode.bean.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (78.99%)
+ * Likes:    1465
+ * Dislikes: 0
+ * Total Accepted:    585.6K
+ * Total Submissions: 737.6K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode leftNode = invertTree(root.left);
+        TreeNode rightNode = invertTree(root.right);
+        root.left = rightNode;
+        root.right = leftNode;
+        return root;
+    }
+}
+// @lc code=end
